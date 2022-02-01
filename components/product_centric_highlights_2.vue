4b70592e6d886e80261de70902af6235ff00b634
jobincleetus <jobincleetus5@gmail.com>
2022-02-01 10:41:01 +0530

    fix

--- components/product_centric_highlights_2.vue
+++ components/product_centric_highlights_2.vue
@@ -72,6 +72,112 @@
 export default {
     name: 'product-centric-highlights-2',
     mounted() {
+
+        function init() {
+
+            if($('.page-container').hasClass( "dark-header" )) {
+                $('.navbar-container').addClass( "dark-nav" )
+            } else{
+                $('.navbar-container').removeClass( "dark-nav" )
+            }
+            
+            let splitWords = function (selector) {
+                var elements = document.querySelectorAll(selector);
+
+                elements.forEach(function (el) {
+                el.dataset.splitText = el.textContent;
+                el.innerHTML = el.textContent
+                    .split(/\s/)
+                    .map(function (word) {
+                    return word
+                        .split("-")
+                        .map(function (word) {
+                        return '<span class="word">' + word + "</span>";
+                        })
+                        .join('<span class="hyphen">-</span>');
+                    })
+                    .join('<span class="whitespace"> </span>');
+                });
+            };
+
+            let splitLines = function (selector) {
+                var elements = document.querySelectorAll(selector);
+
+                splitWords(selector);
+
+                elements.forEach(function (el) {
+                var lines = getLines(el);
+
+                var wrappedLines = "";
+                lines.forEach(function (wordsArr) {
+                    wrappedLines += '<span class="line"><span class="words">';
+                    wordsArr.forEach(function (word) {
+                    wrappedLines += word.outerHTML;
+                    });
+                    wrappedLines += "</span></span>";
+                });
+                el.innerHTML = wrappedLines;
+                });
+            };
+
+            let getLines = function (el) {
+                var lines = [];
+                var line;
+                var words = el.querySelectorAll("span");
+                var lastTop;
+                for (var i = 0; i < words.length; i++) {
+                var word = words[i];
+                if (word.offsetTop != lastTop) {
+                    // Don't start with whitespace
+                    if (!word.classList.contains("whitespace")) {
+                    lastTop = word.offsetTop;
+
+                    line = [];
+                    lines.push(line);
+                    }
+                }
+                line.push(word);
+                }
+                return lines;
+            };
+
+            splitLines(".reveal-text");
+
+            let revealText = document.querySelectorAll(".reveal-text");
+
+            gsap.registerPlugin(ScrollTrigger);
+            // ScrollTrigger.enable();
+
+                let revealLines = revealText.forEach((element, i) => {
+                    const lines = element.querySelectorAll(".words");
+
+                    function addActive() {
+                    $(element).parents("section").addClass("active");
+                    }
+                            
+                    let tl = gsap.timeline({
+                    onComplete: function(){ 
+                        addActive();
+                    },
+                    scrollTrigger: {
+                        id: "indexPannels" + i,
+                        trigger: element,
+                        toggleActions: "restart none none reset"
+                    }
+                    });
+                    tl.set(element, { autoAlpha: 1 });
+                    tl.from(lines, 1, {
+                    yPercent: 100,
+                    ease: Power3.out,
+                    stagger: 0.25,
+                    delay: 0.2
+                    });
+
+                });
+                    console.log("init started")
+        }
+
+        init();
         
         var dispHeight = window.innerHeight;
 
