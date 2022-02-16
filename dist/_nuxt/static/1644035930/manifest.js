017dbd85ec480491234dcaaa9d0346a3d8105fed
jobincleetus <jobincleetus5@gmail.com>
2022-02-16 08:29:50 +0530

    new fixes

--- dist/_nuxt/static/1644035930/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fabout","\u002Fcontact","\u002Ferror","\u002Fproducts\u002Fmeditate","\u002Fprivacy-policy","\u002Fproducts\u002Fmeditate - Copy","\u002Fproducts","\u002Fterms-of-use"]})
---
+++ dist/_nuxt/static/1644980363/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fabout","\u002Ferror","\u002Fcontact","\u002Fproducts\u002Fmeditate - Copy","\u002Fprivacy-policy","\u002Fproducts\u002Fmeditate","\u002Fterms-of-use","\u002Fproducts"]})
